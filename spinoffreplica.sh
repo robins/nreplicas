06d98766f72a8ced974cff0988c08d0943c80c55
Robins Tharakan aws4 <tharakan@gmail.com>
2019-04-15 13:46:43 +0000

    Not Replica is streaming from Master. Mostly working

--- spinoffreplica.sh
+++ spinoffreplica.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+basedir=/home/ubuntu/projects/nreplicas
 bindir=/opt/postgres/master/bin
-datadir=/home/ubuntu/projects/nreplicas/data_nested
+datadir=${basedir}/data_nested
 start_port=6000
 debug=off
+
+function destroy_engine() {
+        port=${1}
+        replica_dir=${datadir}/data${port}
+
+        echo Destroying Engine on Port:${port}
+        cd ${datadir}
+        rm -rf data${port}
+	cd ${basedir}
+}
 
 function create_master() {
 	port=${1}
@@ -11,13 +22,13 @@ function create_master() {
 
 	if [ -d ${this_dir} ]
 	then
-		echo Skipping DataDir creation:${this_dir}
-		return 0
+		destroy_engine ${port}
 	fi
 	echo Creating DataDir for Port:${port}
 	mkdir -p ${datadir}/data${port}
-	${bindir}/initdb -D ${datadir}/data${port} >/dev/null
+	${bindir}/initdb --auth-host=trust --auth-local=trust -D ${datadir}/data${port} >/dev/null
 	sed -i "s/#port = /port = ${port}#/g" ${datadir}/data${port}/postgresql.conf
+
 }
 
 function create_replica() {
@@ -25,8 +36,13 @@ function create_replica() {
 	master_port=${2}
 	replica_dir=${datadir}/data${port}
 
+        if [ -d ${replica_dir} ]
+        then
+                destroy_engine ${port}
+        fi
+
         echo Creating Replica for Port:${port}
-	${bindir}/pg_basebackup -p ${master_port} -D ${replica_dir}
+	${bindir}/pg_basebackup -R -p ${master_port} -D ${replica_dir}
 	sed -i "s/port = /port = ${port}#/g" ${datadir}/data${port}/postgresql.conf
 }
 
@@ -37,12 +53,22 @@ function start_engine() {
 }
 
 function is_up_engine() {
+        port=${1}
+        is_working=`${bindir}/psql -Atqc "select 1;" -p ${port} postgres`
+        if [ ${is_working} -ne '1' ]; then
+                echo "Health Check on Port ${port}:Down"
+        else
+                echo "Health Check on Port ${port}:Up"
+        fi
+}
+
+function is_up_replica() {
 	port=${1}
-	is_working=`${bindir}/psql -Atqc "select 1;" -p ${port} postgres`
-	if [ ${is_working} -ne '1' ]; then
-		echo "Health Check on Port ${port}:Down"
+	is_working=`${bindir}/psql -Atqc "select pg_is_in_recovery();" -p ${port} postgres`
+	if [ ${is_working} != 't' ]; then
+		echo "Is Replicating on Port ${port}:Down (${is_working})"
 	else
-		echo "Health Check on Port ${port}:Up"
+		echo "Is Replicating on Port ${port}:Up (${is_working})"
 	fi
 }
 
@@ -54,19 +80,24 @@ function stop_engine() {
 
 function initiate_n_replicas() {
 	count=${1}
+	master_port=${start_port}
 
 	create_master ${start_port}
 	start_engine ${start_port}
 	is_up_engine ${start_port}
+#	psql -U postgres -d postgres -c "CREATE USER replication WITH replication ENCRYPTED PASSWORD 'changeme' LOGIN"
 
 	for i in `seq 1 ${count}`;
 	do
 		port="$((${start_port}+${i}))"
 		echo Initiate Replica on Port:${port}
-		create_replica ${port} ${start_port}
+		create_replica ${port} ${master_port}
 		start_engine ${port}
 		is_up_engine ${port}
+		is_up_replica ${port}
+		master_port=${port}
 	done
+
 }
 
 function shutdown_n_replicas() {
@@ -75,9 +106,11 @@ function shutdown_n_replicas() {
 	do
 		port="$((${start_port}+${i}))"
 		stop_engine ${port}
+#		destroy_engine ${port}
 	done
 	stop_engine ${start_port}
+#	destroy_engine ${start_port}
 }
 
 initiate_n_replicas 1
-#shutdown_n_replicas 1
+shutdown_n_replicas 1
