c9778d6098542c9f64326afdc1fda541ad11bec1
Robins Tharakan aws4 <tharakan@gmail.com>
2019-04-15 11:27:06 +0000

    WIP. Partly done recovering standby from master. Still recoveryconf pending

--- spinoffreplica.sh
+++ spinoffreplica.sh
@@ -5,9 +5,10 @@ datadir=/home/ubuntu/projects/nreplicas/data_nested
 start_port=6000
 debug=off
 
-function create_data_dir() {
+function create_master() {
 	port=${1}
 	this_dir=${datadir}/data${port}
+
 	if [ -d ${this_dir} ]
 	then
 		echo Skipping DataDir creation:${this_dir}
@@ -16,17 +17,26 @@ function create_data_dir() {
 	echo Creating DataDir for Port:${port}
 	mkdir -p ${datadir}/data${port}
 	${bindir}/initdb -D ${datadir}/data${port} >/dev/null
-	sed -i "s/#port = 5432/port = ${port}/g" ${datadir}/data${port}/postgresql.conf
+	sed -i "s/#port = /port = ${port}#/g" ${datadir}/data${port}/postgresql.conf
 }
 
+function create_replica() {
+        port=${1}
+	master_port=${2}
+	replica_dir=${datadir}/data${port}
 
-function start_replica() {
+        echo Creating Replica for Port:${port}
+	${bindir}/pg_basebackup -p ${master_port} -D ${replica_dir}
+	sed -i "s/port = /port = ${port}#/g" ${datadir}/data${port}/postgresql.conf
+}
+
+function start_engine() {
 	port=${1}
-	echo Starting Relica on Port:${port}
+	echo Starting Engine on Port:${port}
 	${bindir}/pg_ctl -D ${datadir}/data${port} -l logfiles/logfile${port} start >/dev/null
 }
 
-function is_up_replica() {
+function is_up_engine() {
 	port=${1}
 	is_working=`${bindir}/psql -Atqc "select 1;" -p ${port} postgres`
 	if [ ${is_working} -ne '1' ]; then
@@ -36,21 +46,26 @@ function is_up_replica() {
 	fi
 }
 
-function stop_replica() {
+function stop_engine() {
 	port=${1}
-	echo Stopping Replica on Port:${port}
+	echo Stopping Engine on Port:${port}
 	${bindir}/pg_ctl -D ${datadir}/data${port} -l logfiles/logfile${port} stop >/dev/null
 }
 
 function initiate_n_replicas() {
 	count=${1}
+
+	create_master ${start_port}
+	start_engine ${start_port}
+	is_up_engine ${start_port}
+
 	for i in `seq 1 ${count}`;
 	do
 		port="$((${start_port}+${i}))"
 		echo Initiate Replica on Port:${port}
-		create_data_dir ${port}
-		start_replica ${port}
-		is_up_replica ${port}
+		create_replica ${port} ${start_port}
+		start_engine ${port}
+		is_up_engine ${port}
 	done
 }
 
@@ -59,10 +74,10 @@ function shutdown_n_replicas() {
 	for i in `seq 1 ${count}`;
 	do
 		port="$((${start_port}+${i}))"
-		stop_replica ${port}
-		echo Shutdown Replica on Port:${port}
+		stop_engine ${port}
 	done
+	stop_engine ${start_port}
 }
 
-initiate_n_replicas 2
-shutdown_n_replicas 2
+initiate_n_replicas 1
+#shutdown_n_replicas 1
