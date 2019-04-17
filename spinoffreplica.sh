cff242ff6ed54c15ed69056ea6a85c3b5216a491
Robins Tharakan aws4 <tharakan@gmail.com>
2019-04-17 00:07:50 +0000

    Add some basic Logging infra + remove a redundant stop_engine on the master

--- spinoffreplica.sh
+++ spinoffreplica.sh
@@ -4,12 +4,24 @@ basedir=/home/ubuntu/projects/nreplicas
 bindir=/opt/postgres/master/bin
 datadir=${basedir}/data_nested
 start_port=6000
-debug=off
+
+debug_level=20
+# 10 Errors
+# 15 Initiate / Shutdown
+# 20 Above + Individual Replica Starting / Stopping / Destroy
+# 25 Above + Some more DONE messages
+# 30 Above + Pending action items
+
+
+function decho () {
+	[[ ${2} -le ${debug_level} ]] && echo ${1}
+}
 
 function stop_engine() {
         port=${1}
-        echo Stopping Engine on Port:${port}
+        decho "Stopping Engine on Port:${port}" 20
         ${bindir}/pg_ctl -D ${datadir}/data${port} -m immediate -l logfiles/logfile${port} stop >/dev/null
+        decho "Stopping Engine on Port:${port} DONE" 25
 }
 
 function stop_all_engines() {
@@ -29,13 +41,15 @@ function destroy_engine() {
         port=${1}
         replica_dir=${datadir}/data${port}
 
-        echo Destroying Engine on Port:${port}
 	if [ -f ${replica_dir}/postmaster.pid ]; then
 		stop_engine ${port}
 	fi
+
+        decho "Destroying Engine on Port:${port}" 20
         cd ${datadir}
-        rm -rf data${port}
+        rm -rf data${port} &
 	cd ${basedir}
+        decho "Destroying Engine on Port:${port} DONE" 25
 }
 
 function create_master() {
@@ -46,12 +60,13 @@ function create_master() {
 	then
 		destroy_engine ${port}
 	fi
-	echo Creating DataDir for Port:${port}
+
+	decho "Creating DataDir for Port:${port}" 20
 	mkdir -p ${datadir}/data${port}
 	${bindir}/initdb --auth-host=trust --auth-local=trust -D ${datadir}/data${port} >/dev/null
 	sed -i "s/#port = /port = ${port}#/g" ${datadir}/data${port}/postgresql.conf
-	sed -i "s/shared_buffers = 128MB/shared_buffers = 256kB/g" ${datadir}/data${port}/postgresql.conf
-
+	sed -i "s/shared_buffers = 128MB/shared_buffers = 128kB/g" ${datadir}/data${port}/postgresql.conf
+	decho "Creating DataDir for Port:${port} DONE" 25
 }
 
 function create_replica() {
@@ -64,70 +79,83 @@ function create_replica() {
                 destroy_engine ${port}
         fi
 
-        echo Creating Replica for Port:${port}
+        decho "Creating Replica for Port:${port}" 20
 	${bindir}/pg_basebackup -R -p ${master_port} -D ${replica_dir}
 	sed -i "s/port = /port = ${port}#/g" ${datadir}/data${port}/postgresql.conf
+        decho "Creating Replica for Port:${port} DONE" 25
 }
 
 function start_engine() {
 	port=${1}
-	echo Starting Engine on Port:${port}
+
+	decho "Starting Engine on Port:${port}" 20
 	${bindir}/pg_ctl -D ${datadir}/data${port} -l logfiles/logfile${port} start >/dev/null
+	decho "Starting Engine on Port:${port} DONE" 25
 }
 
 function is_up_engine() {
         port=${1}
-        is_working=`${bindir}/psql -Atqc "select 1;" -p ${port} postgres`
-        if [ ${is_working} -ne '1' ]; then
-                echo "Health Check on Port ${port}:Down"
-        else
-                echo "Health Check on Port ${port}:Up"
-        fi
+        is_working=`${bindir}/psql -Atqc "select 1;" -p ${port} postgres 2>/dev/null`
+        if [ ! -z ${is_working} ]; then
+        	if [ ${is_working} -ne '1' ]; then
+                	decho "Health Check on Port ${port}:Down" 10
+        	else
+                	decho "Health Check on Port ${port}:Up" 30
+        	fi
+		decho "We do nothing here?" 30
+	fi
 }
 
 function is_up_replica() {
 	port=${1}
-	is_working=`${bindir}/psql -Atqc "select pg_is_in_recovery();" -p ${port} postgres`
-	if [ ${is_working} != 't' ]; then
-		echo "Is Replicating on Port ${port}:Down (${is_working})"
-	else
-		echo "Is Replicating on Port ${port}:Up (${is_working})"
+	is_working=`${bindir}/psql -Atqc "select pg_is_in_recovery();" -p ${port} postgres 2>/dev/null`
+	if [ ! -z ${is_working} ]; then
+		if [ ${is_working} != 't' ]; then
+			decho "Is Replicating on Port ${port}:Down (${is_working})" 10
+		else
+			decho "Is Replicating on Port ${port}:Up (${is_working})" 30
+		fi
+		decho "We do nothing here 2?" 30
 	fi
 }
 
 function initiate_n_replicas() {
 	count=${1}
+
+	decho "Initiating all Replicas" 11
 	master_port=${start_port}
 
 	create_master ${start_port}
 	start_engine ${start_port}
 	is_up_engine ${start_port}
-#	psql -U postgres -d postgres -c "CREATE USER replication WITH replication ENCRYPTED PASSWORD 'changeme' LOGIN"
 
 	for i in `seq 1 ${count}`;
 	do
 		port="$((${start_port}+${i}))"
-		echo Initiate Replica on Port:${port}
+
+		decho "Initiate Replica on Port:${port}" 16
 		create_replica ${port} ${master_port}
 		start_engine ${port}
 		is_up_engine ${port}
 		is_up_replica ${port}
 		master_port=${port}
+		decho "Initiate Replica on Port:${port} DONE" 19
 	done
-
+	decho "Initiating all Replicas DONE" 14
 }
 
 function shutdown_n_replicas() {
 	count=${1}
+	decho "Shutting down all Replicas" 11
 	for i in `seq ${count} -1 1`;
 	do
 		port="$((${start_port}+${i}))"
 		destroy_engine ${port}
 	done
-	stop_engine ${start_port}
 	destroy_engine ${start_port}
+	decho "Shutting down all Replicas DONE" 14
 }
 
 replica_count=1000
-initiate_n_replicas ${replica_count}
-#shutdown_n_replicas ${replica_count}
+#initiate_n_replicas ${replica_count}
+shutdown_n_replicas ${replica_count}
